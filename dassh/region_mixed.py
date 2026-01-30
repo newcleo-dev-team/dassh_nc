5eb58d844f4fe3790d712f01ccf3cc034741e8a9
Francesco Pepe <francesco.pepe@polito.it>
2026-01-30 11:12:21 +0100

    [027_mixed_convection], issue #27, modified wall convection term and balance

--- dassh/region_mixed.py
+++ dassh/region_mixed.py
@@ -310,7 +310,7 @@ class MixedRegion(RoddedRegion):
             # Error introduced in the energy balance by h_star approximation
             delta_m = self.sc_mfr - old_mfr
             star_error = self._hstar * delta_m
-            self.update_ebal(dz*np.sum(qq), self._qw * dz, mcpdT_i, star_error)
+            self.update_ebal(dz*np.sum(qq), self._qw, mcpdT_i, star_error)
             
             
     def _copy_solution(self, drho: np.ndarray, dv: np.ndarray, 
@@ -366,8 +366,8 @@ class MixedRegion(RoddedRegion):
         energy_b = qq * dz / self.params['area'][self.subchannel.type[:nn]] \
             + EEX
         # Wall convection term
-        self._qw = self._wall_convection() # [W/m]
-        energy_b[self.ht['conv']['ind']] += self._qw * dz / self.params['area'][
+        self._qw = self._wall_convection() * dz 
+        energy_b[self.ht['conv']['ind']] += self._qw  / self.params['area'][
             self.subchannel.type[:nn][self.ht['conv']['ind']]]
         # Build momentum terms of the known vector
         momentum_b = GG + MEX 
